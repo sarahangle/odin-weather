fa7a61e644cbaf6cdea3b7d3e13933d3ea25faf3
sarahangle <sna44@cornell.edu>
2023-05-09 23:05:44 -0700

    Create header UI. Add local forecast using geolocation.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,23 @@
 import './style.css';
 import { getRealtimeFromLocationString, getForecastFromLocationString } from './weatherAPI';
 
-getRealtimeFromLocationString('london');
-getForecastFromLocationString('New York City', 5);
+const initForm = () => {
+  document.getElementById('location-search-btn').addEventListener('click', (evt) => {
+    evt.preventDefault();
+    if (document.getElementById('location-text').value) {
+      getForecastFromLocationString(document.getElementById('location-text').value);
+      document.getElementById('location-text').value = '';
+    }
+  });
+};
+
+const showLocalForecast = (pos) => {
+  getForecastFromLocationString(`${pos.coords.latitude}, ${pos.coords.longitude}`);
+};
+
+const showDefaultForecast = () => {
+  getForecastFromLocationString('New York City');
+};
+
+initForm();
+navigator.geolocation.getCurrentPosition(showLocalForecast, showDefaultForecast);
--- src/weatherAPI.js
+++ src/weatherAPI.js
@@ -1,55 +1,67 @@
 const weatherAPIKey = 'ed6020a452a5438e9d3183415230805';
 
 async function getRealtimeFromLocationString(locationString) {
-  const requestString = `https://api.weatherapi.com/v1/current.json?key=${weatherAPIKey}&q=${locationString}`;
-  const dataPromise = await fetch(requestString, { mode: 'cors' });
-  const dataJSON = await dataPromise.json();
-  const data = {
-    location: dataJSON.location.name,
-    time: dataJSON.location.localtime,
-    condition_text: dataJSON.current.condition.text,
-    temperature_f: dataJSON.current.temp_f,
-    temperature_c: dataJSON.current.temp_c,
-    feelslike_f: dataJSON.current.feelslike_f,
-    feelslike_c: dataJSON.current.feelslike_c,
-    wind_mph: dataJSON.current.wind_mph,
-    wind_kph: dataJSON.current.wind_kph,
-    wind_dir: dataJSON.current.wind_dir,
-    humidity: dataJSON.current.humidity,
-  };
-  console.log(data);
+  try {
+    const requestString = `https://api.weatherapi.com/v1/current.json?key=${weatherAPIKey}&q=${locationString}`;
+    const dataPromise = await fetch(requestString, { mode: 'cors' });
+    if (!dataPromise.ok) { throw new Error('Location not found!'); }
+    const dataJSON = await dataPromise.json();
+    const data = {
+      current: {
+        location: dataJSON.location.name,
+        time: dataJSON.location.localtime,
+        condition_text: dataJSON.current.condition.text,
+        temperature_f: dataJSON.current.temp_f,
+        temperature_c: dataJSON.current.temp_c,
+        feelslike_f: dataJSON.current.feelslike_f,
+        feelslike_c: dataJSON.current.feelslike_c,
+        wind_mph: dataJSON.current.wind_mph,
+        wind_kph: dataJSON.current.wind_kph,
+        wind_dir: dataJSON.current.wind_dir,
+        humidity: dataJSON.current.humidity,
+      },
+    };
+    console.log(data);
+  } catch (err) {
+    console.log(err);
+  }
 }
 
 async function getForecastFromLocationString(locationString, numDays) {
-  const requestString = `https://api.weatherapi.com/v1/forecast.json?key=${weatherAPIKey}&q=${locationString}&days=${numDays}`;
-  const dataPromise = await fetch(requestString, { mode: 'cors' });
-  const dataJSON = await dataPromise.json();
-  const data = {
-    current: {
-      location: dataJSON.location.name,
-      time: dataJSON.location.localtime,
-      condition_text: dataJSON.current.condition.text,
-      temperature_f: dataJSON.current.temp_f,
-      temperature_c: dataJSON.current.temp_c,
-      feelslike_f: dataJSON.current.feelslike_f,
-      feelslike_c: dataJSON.current.feelslike_c,
-      wind_mph: dataJSON.current.wind_mph,
-      wind_kph: dataJSON.current.wind_kph,
-      wind_dir: dataJSON.current.wind_dir,
-      humidity: dataJSON.current.humidity,
-    },
-    forecast: [],
-  };
-  dataJSON.forecast.forecastday.forEach((val) => {
-    const entry = {};
-    entry.condition = val.day.condition.text;
-    entry.maxtemp_f = val.day.maxtemp_f;
-    entry.maxtemp_c = val.day.maxtemp_c;
-    entry.mintemp_f = val.day.mintemp_f;
-    entry.mintemp_c = val.day.mintemp_c;
-    data.forecast.push(entry);
-  });
-  console.log(data);
+  try {
+    const requestString = `https://api.weatherapi.com/v1/forecast.json?key=${weatherAPIKey}&q=${locationString}&days=${numDays}`;
+    const dataPromise = await fetch(requestString, { mode: 'cors' });
+    if (!dataPromise.ok) { throw new Error('Location not found!'); }
+    const dataJSON = await dataPromise.json();
+    const data = {
+      current: {
+        location: dataJSON.location.name,
+        time: dataJSON.location.localtime,
+        condition_text: dataJSON.current.condition.text,
+        temperature_f: dataJSON.current.temp_f,
+        temperature_c: dataJSON.current.temp_c,
+        feelslike_f: dataJSON.current.feelslike_f,
+        feelslike_c: dataJSON.current.feelslike_c,
+        wind_mph: dataJSON.current.wind_mph,
+        wind_kph: dataJSON.current.wind_kph,
+        wind_dir: dataJSON.current.wind_dir,
+        humidity: dataJSON.current.humidity,
+      },
+      forecast: [],
+    };
+    dataJSON.forecast.forecastday.forEach((val) => {
+      const entry = {};
+      entry.condition = val.day.condition.text;
+      entry.maxtemp_f = val.day.maxtemp_f;
+      entry.maxtemp_c = val.day.maxtemp_c;
+      entry.mintemp_f = val.day.mintemp_f;
+      entry.mintemp_c = val.day.mintemp_c;
+      data.forecast.push(entry);
+    });
+    console.log(data);
+  } catch (err) {
+    console.log(err);
+  }
 }
 
 export { getRealtimeFromLocationString, getForecastFromLocationString };
